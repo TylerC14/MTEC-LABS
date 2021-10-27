e33bc31a388880e7aedfb59d768c7a368392c008
Tyler Christensen <tyler.chris@live.com>
2021-10-27 10:32:07 -0600

    HomeFurnitureLabStart

--- Student 2/1 - Tables and Persistence/8 - System View Controllers/lab/HomeFurniture/HomeFurniture/FurnitureDetailViewController.swift
+++ Student 2/1 - Tables and Persistence/8 - System View Controllers/lab/HomeFurniture/HomeFurniture/FurnitureDetailViewController.swift
@@ -1,7 +1,7 @@
 
 import UIKit
 
-class FurnitureDetailViewController: UIViewController {
+class FurnitureDetailViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     var furniture: Furniture?
     
@@ -39,11 +39,49 @@ class FurnitureDetailViewController: UIViewController {
     }
     
     @IBAction func choosePhotoButtonTapped(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: {(_) in imagePicker.sourceType = .photoLibrary})
+            
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+                print("Camera button was clicked")
+            }
+            
+            
+            alertController.addAction(cancelAction)
+            alertController.addAction(cameraAction)
+            alertController.addAction(photoLibraryAction)
+            
+            present(alertController, animated: true, completion: nil)
+        }
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        
+        photoImageView.image = selectedImage
+        
+        if let image = photoImageView.image, let jpegData = image.jpegData(compressionQuality: 0.9) {
+            furniture?.imageData = jpegData
+        }
+        
+        dismiss(animated: true, completion: nil)
+        updateView()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func actionButtonTapped(_ sender: Any) {
         
     }
     
 }
+
